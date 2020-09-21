e26e1a1aed8cc57cd237e27a986ae9d4ccf5a388
MACOS <macos@MacBook-Pro-de-MACOS.local>
2020-09-21 20:13:21 +0200

    basic seo added

--- src/components/ClientsView.js
+++ src/components/ClientsView.js
@@ -5,73 +5,8 @@ import { Parallax } from 'react-parallax'
 import Slide from 'react-reveal/Slide'
 
 import Colors from '../constants/Colors';
+import { topics } from '../constants/Constants'
 import { Icon, Card, Reveal, Image, Button, Modal } from 'semantic-ui-react';
-
-const topics = [
-    {
-        title: "Application web",
-        icon: "laptop",
-        subtitle: "outils professionnels",
-        imageSrc: "https://cdn.searchenginejournal.com/wp-content/uploads/2019/12/5-ways-seo-web-design-go-together-5e2945dd5df37.png",
-        description: `Vous allez gagner du temps !
-        Conçevons les outils web dont votre activité a besoin.
-        Un intranet d'entreprise ? un système d'organisation ? 
-        On peut vous faire ça !`
-    },
-    {
-        title: "Application mobile",
-        icon: "mobile alternate",
-        subtitle: "privée, ios & android",
-        imageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvCRL3L00-yvWcDm7GfGWFBrFswxj9_QdoVA&usqp=CAU",
-        description: `Mettez toute votre entreprise dans votre poche et partagez vos outils personnalisés avec vos collaborateurs via l'AppStore Apple et le PlayStore Google`
-    },
-    {
-        title: "Site vitrine",
-        icon: "sitemap", 
-        subtitle: "web & responsive mobile",
-        imageSrc: "https://www.poush.be/wp-content/uploads/2019/02/blog-responsive-design.jpg",
-        description: `Un produit, un service ou un évènement à présenter et/ou à mettre en avant ? Vos visiteurs vont faire du lèche-vitrine, on va faire briller les étagères !`
-    },
-    {
-        title: "Base de données",
-        icon: "database",        
-        subtitle: "cryptage & stockage",
-        imageSrc: "https://www.masterdc.com/images/photos/dedicated-and-managed-server-differences.jpg",
-        description: `Il parait que les datas valent de l'or, autant les mettre à l'abris ! Nous proposons des solutions de stockage sécurisé à court, moyen et long terme`
-    },
-    {
-        title: "Blockchain",
-        icon: "ethereum",
-        subtitle: "smart-contracts & applications décentralisées",
-        imageSrc: "https://www.everteam.com/wp-content/uploads/2019/02/600shutterstock_1061262521.jpg",
-        description: `La dernière grosse révolution en matière d'internet est bel est bien la blockchain. Fini les serveurs, tokénisation et décentralisation sont les mots d'ordre ici.`
-    },
-    {
-        title: "Conception graphique",
-        icon: "paint brush",
-        subtitle: "logos, flyers, bannières & cartes de visite",
-        imageSrc: "https://pbs.twimg.com/media/DZS4d8lX0AADrc7.jpg",
-        description: `Besoin d'une réalisation graphique personnalisée dans un format précis ? on s'en charge dans le respect de votre charte graphique et s'il faut, on la conçoit ensemble.`
-    },
-    // audit
-    {
-        title: "Audit de code",
-        icon: "eye",
-        subtitle: "qualité, sureté & sécurité",
-        imageSrc: "https://kinsta.com/fr/wp-content/uploads/sites/4/2020/03/gardez-vos-retours-instructifs.jpg",
-        description: `Besoin d'un point de vue extérieur ? Nous faisons le tour de votre code et vous rendons un rapport détaillé pour permettre à vos collaborateurs d'apprécier son niveau de qualité et de sécurité`
-    },
-    // formation
-    {
-        title: "Formation & Mentorat",
-        icon: "student",
-        subtitle: "conception, languages & technologies",
-        imageSrc: "https://img5.leboncoin.fr/ad-image/391947a5778ace94c458aeaf556603a25a7a84cd.jpg",
-        description: `Besoin d'une connaissance technique ? Chez ProApps on aime partager, nous assurons des modules de formation personnalisés sur demande, donc il suffit de nous demander ;)`
-    },
-]
-
-
 
  
 const ClientsView = () => {
@@ -136,6 +71,8 @@ const ClientsView = () => {
             </div>
         </Slide>
         <Modal
+            //size="fullscreen"
+            //style={{ height: '90vh', width: '90vw' }}
             open={topicView >= 0}
             header={topicView >= 0 && topics[topicView].title}
             content={<div style={{padding: 20}}><h4>Le site est en construction,</h4> Il n'y a pas plus d'informations ici pour le moment, vous pouvez toujours nous contacter :<br /><ul><li>Par téléphone au 07 66 40 32 95</li><li>Par email à <a href="mailto:contact@proapps.fr">contact@proapps.fr</a></li></ul></div>}
